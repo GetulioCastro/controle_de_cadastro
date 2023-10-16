014af1b351d75af5c30a708d4b413f38aa42fd0c
Getulio Augusto Ferreira de Castro Junior <augustoge71@gmail.com>
2023-10-16 18:09:10 -0300

    Projeto Asp.NET com MVC para aprofundamento.

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCadastro.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
